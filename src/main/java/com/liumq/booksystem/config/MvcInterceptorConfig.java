eac925c22f3eeca7ac1fba7c8c874599c2d7f5b3
liumq-95 <495800763@qq.com>
2020-03-08 18:54:39 +0800

    add fime

--- src/main/java/com/liumq/booksystem/config/MvcInterceptorConfig.java
+++ src/main/java/com/liumq/booksystem/config/MvcInterceptorConfig.java
@@ -12,6 +12,7 @@ public class MvcInterceptorConfig  extends WebMvcConfigurationSupport {
     @Override
     protected void addInterceptors(InterceptorRegistry registry) {
         System.out.println("==========MyInterceptor===========");
+        //取消拦截静态文件，否则样式加载不出来
         registry.addInterceptor(myInterceptor).addPathPatterns("/**").excludePathPatterns("/static/**","/jq/**","/layui-v2.5.6/**","/Vue/**");
 
         super.addInterceptors(registry);

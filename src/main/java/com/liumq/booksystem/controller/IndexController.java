d0f20c6d3863f98e0622d380f9ed99b93d89a769
liumq-95 <495800763@qq.com>
2020-03-16 19:36:20 +0800

    11111111111

--- src/main/java/com/liumq/booksystem/controller/IndexController.java
+++ src/main/java/com/liumq/booksystem/controller/IndexController.java
@@ -1,22 +1,41 @@
 package com.liumq.booksystem.controller;
 
+import com.liumq.booksystem.entity.Menu;
+import com.liumq.booksystem.entity.RoleMenu;
+import com.liumq.booksystem.entity.User;
+import com.liumq.booksystem.service.MenuService;
+import com.liumq.booksystem.service.RoleMenuService;
 import com.liumq.booksystem.util.BrowserUtil;
+import net.sf.json.JSON;
+import net.sf.json.JSONObject;
+import org.apache.shiro.SecurityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.annotation.Resource;
 import javax.jws.WebParam;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class IndexController {
 
     @Autowired
     private ServletContext servletContext;
+
+    @Resource
+    private MenuService menuService;
+
+    @Resource
+    private RoleMenuService roleMenuService;
 
     @RequestMapping("/")
     public String index_1(HttpServletResponse res, HttpServletRequest req) {
@@ -43,6 +62,38 @@ public class IndexController {
         ModelAndView mav = new ModelAndView();
         String UserAgent = req.getHeader("User-Agent");
         mav.setViewName("/admin/main");
+
+        //使用shiro 从session 中得到user信息
+        User currentUser = (User) SecurityUtils.getSubject().getSession().getAttribute("currentUser");
+        if (currentUser.getRole() == null) {
+            return mav;
+        }
+        //根据当前用户对应角色展示菜单
+        Map<String, Object> map = new HashMap<>();
+        map.put("pId", -1);
+        List<Menu> menuList = menuService.list(map, 0, 100);
+        List<JSONObject> list = new ArrayList<>();
+        for (Menu menu : menuList) {
+            RoleMenu roleMenu = roleMenuService.findByRoleIdAndMenuId(currentUser.getRole().getId(), menu.getId());
+            if(roleMenu!=null)
+            {
+                JSONObject node = new JSONObject();
+                node.put("id",menu.getId());
+                node.put("text",menu.getName());
+                node.put("url",menu.getUrl());
+                node.put("type",menu.getType());
+                node.put("icon",menu.getIcon());
+                node.put("divId",menu.getDivId());
+                node.put("children",getChildren(menu.getId(),currentUser.getRole().getId()));
+                list.add(node);
+            }
+
+        }
+
         return mav;
+    }
+
+    private Object getChildren(Integer pId, Integer roleId) throws Exception {
+        return null;
     }
 }

e9588d901c08caa78e2a171d375d9a63ffc6fb8c
liumq-95 <495800763@qq.com>
2020-03-19 10:18:41 +0800

    ...

--- src/main/java/com/liumq/booksystem/config/ErrorPageConfig.java
+++ src/main/java/com/liumq/booksystem/config/ErrorPageConfig.java
@@ -11,5 +11,7 @@ public class ErrorPageConfig implements ErrorPageRegistrar {
         ErrorPage e404 = new ErrorPage(HttpStatus.NOT_FOUND,"/error/404");
 
         ErrorPage e500 = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR,"/error/500");
+
+        registry.addErrorPages(e404,e500);
     }
 }
